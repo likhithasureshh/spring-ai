6ba8629fd6680902dbba57e78b185340d5e2314b
Likitha Suresh <likithasuresh@Likithas-MacBook-Air.local>
2026-06-13 14:50:08 +0530

    added embedding and vector store

--- src/main/java/com/spring_ai/learn_spring_ai/service/AIService.java
+++ src/main/java/com/spring_ai/learn_spring_ai/service/AIService.java
@@ -5,8 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -14,6 +19,58 @@ import java.util.Map;
 
 public class AIService {
     private final ChatClient chatClient;
+    private final EmbeddingModel embeddingModel;
+    private final VectorStore vectorStore;
+
+
+    public float[] getEmbedding(String text)
+    {
+        return embeddingModel.embed(text);
+    }
+
+    public void storeInVectorDb()
+    {
+        Document doc1 = new Document(
+                "Spring Boot is a Java framework used to build standalone, production-ready applications...",
+                Map.of(
+                        "id", "doc_001",
+                        "title", "Introduction to Spring Boot",
+                        "category", "Java"
+                )
+        );
+
+        Document doc2 = new Document(
+                "A vector database stores data as numerical embeddings...",
+                Map.of(
+                        "id", "doc_002",
+                        "title", "Understanding Vector Databases",
+                        "category", "AI"
+                )
+        );
+
+        Document doc3 = new Document(
+                "Retrieval-Augmented Generation combines information retrieval with large language models...",
+                Map.of(
+                        "id", "doc_003",
+                        "title", "Introduction to RAG",
+                        "category", "Generative AI"
+                )
+        );
+        vectorStore.add(List.of(doc1,doc2,doc3));
+
+    }
+
+    public List<Document> similaritySearch(String text)
+    {
+        return vectorStore.similaritySearch(
+                SearchRequest.builder()
+                        .query(text)
+                        .similarityThreshold(0.7)
+                        .topK(1)
+                        .build()
+        );
+    }
+
 
 
 
--- src/test/java/com/spring_ai/learn_spring_ai/service/AIServiceTests.java
+++ src/test/java/com/spring_ai/learn_spring_ai/service/AIServiceTests.java
@@ -1,8 +1,11 @@
 package com.spring_ai.learn_spring_ai.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 public class AIServiceTests {
@@ -14,4 +17,26 @@ public class AIServiceTests {
     {
         System.out.println(aiService.getResult("dogs"));
     }
+    @Test
+    void embedded()
+    {
+        var res=aiService.getEmbedding("Its a big text here");
+        System.out.println(Arrays.toString(res));
+    }
+    @Test
+    void store()
+    {
+        aiService.storeInVectorDb();
+    }
+
+    @Test
+    void search()
+    {
+       var res= aiService.similaritySearch("what is spring boot");
+       for(Document doc : res)
+       {
+           System.out.println(res);
+       }
+    }
+
 }
